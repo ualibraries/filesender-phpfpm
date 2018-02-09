d37a215588f177003b52c43d98c84457564907c7
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-02-09 13:58:20 -0700

    Added template files

--- compose/shibboleth/setup.sh
+++ compose/shibboleth/setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Make sure we are running from the setup.sh directory
+SHELL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SETUP_DIR="$( cd $SHELL_SCRIPT_DIR/ && pwd )"
+while [ ! -d "$SETUP_DIR/template" ]; do
+    SETUP_DIR="$( cd $SETUP_DIR/.. && pwd )"
+done
+
+echo "RUNNING from directory $SETUP_DIR"
+cd $SETUP_DIR
+
 HOSTIP=$1
 
 if [ "$HOSTIP" = "" ]; then
@@ -18,3 +28,39 @@ if [ "$OCTETS" != "4" ]; then
    exit 1
 fi
 
+DEVICE=$HOSTIP
+DESTDIR=shibboleth
+CERT_KEY=sp-key.pem
+CERT_CSR=sp-csr.pem
+CERT_SIGNED=sp-cert.pem
+echo "GENERATING shibboleth self-signed cert files"
+echo "   $DESTDIR/$CERT_SIGNED"
+echo "   $DESTDIR/$CERT_KEY"
+
+SUBJECT="/C=FakeCountry/postalCode=FakeZip/ST=FakeState/L=FakeCity/streetAddress=FakeStreet/O=FakeOrganization/OU=FakeDepartment/CN="
+
+# Create shib private key and csr:
+openssl req -nodes -newkey rsa:2048 -keyout $DESTDIR/$CERT_KEY -subj "$SUBJECT$DEVICE" -out $DESTDIR/$CERT_CSR
+
+# Create shib self-signed cert:
+SIGNING_KEY="-signkey $DESTDIR/$CERT_KEY"
+DAYS=1095   # 3 * 365
+
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:$DEVICE") -in $DESTDIR/$CERT_CSR $SIGNING_KEY -out $DESTDIR/$CERT_SIGNED -days $DAYS -sha256
+
+DESTDIR=nginx/conf.d
+CERT_KEY=nginx-ssl.key
+CERT_CSR=nginx-ssl.csr
+CERT_SIGNED=nginx-ssl.crt
+echo "GENERATING nginx ssl self-signed cert files"
+echo "   $DESTDIR/$CERT_SIGNED"
+echo "   $DESTDIR/$CERT_CSR"
+echo "   $DESTDIR/$CERT_KEY"
+
+# Create nginx private key and csr:
+openssl req -nodes -newkey rsa:2048 -keyout $DESTDIR/$CERT_KEY -subj "$SUBJECT$DEVICE" -out $DESTDIR/$CERT_CSR
+
+# Create nginx self-signed cert:
+SIGNING_KEY="-signkey $DESTDIR/$CERT_KEY"
+
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:$DEVICE") -in $DESTDIR/$CERT_CSR $SIGNING_KEY -out $DESTDIR/$CERT_SIGNED -days $DAYS -sha256
