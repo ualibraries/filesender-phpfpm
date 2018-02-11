fe0ae7d671c4e7ddbcad69a0ed1f37ff46479537
Geoffrey L. Brimhall <geoff@glbrimhall.com>
2018-02-10 20:16:44 -0700

    Have simplesaml example using gmail test account for smtp

--- dbuild.sh
+++ dbuild.sh
@@ -6,7 +6,7 @@ NETWORK=${5:-host}
 TAG=${4:-1.6}
 CONTAINER=${3:-filsender-test}
 REPOSITORY=${2:-filesender-phpfpm}
-ACTION=${1}
+ACTION=${1:-BUILD}
 DAEMONIZE=-d
 
 # Delete test container built from docker file
