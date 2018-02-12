02d73a78a6bbae2b641c5d30576a936aa842f273
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-02-12 15:54:51 -0700

    Latest changes indicating reason for SSL certificate error

--- compose/shibboleth/setup-shib.sh
+++ compose/shibboleth/setup-shib.sh
@@ -132,4 +132,4 @@ echo "RERUN: to redo this setup, delete $METADATA_FILE and re-run ./setup-shib.s
 echo
 echo "REGISTER this shibboleth instance by uploading file $SETUP_DIR/$METADATA_FILE to https://www.testshib.org/register.html#"
 echo
-echo "FINALLY browse to https://$HOSTIP .You will need to accept any error indicating the https ssl cert is invalid or not private"
+echo "FINALLY browse to https://$HOSTIP .You will need to accept any error indicating the https ssl cert is invalid or not private since a self-signed cert is being used instead of an ssl certificate registered with a certificate authority"
