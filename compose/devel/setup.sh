426ea6f0b7c1657fb1a249a7b57dcaa0d365690c
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-07-12 15:54:55 -0700

    Updated to use filesender 2.0

--- compose/devel/setup.sh
+++ compose/devel/setup.sh
@@ -2,6 +2,6 @@
 git clone git@github.com:glbrimhall/filesender.git
 cp git.config filesender/.git/config
 cd filesender
-git checkout directory-tree-upload
+git checkout issue-342-fix
 cd -
 docker-compose up -d
